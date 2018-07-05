160f917f4791001d83475123f7d38f60db46fd79
Valentyn Tulub <valentyntu@softwareplanet.uk.com>
2018-07-05 13:02:36 +0300

    Initial commit

---
+++ src/main/java/com/intelink/compproj/Application.java
@@ -0,0 +1,8 @@
+package com.intelink.compproj;
+
+public class Application {
+
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/main/java/com/intelink/compproj/entity/Project.java
@@ -0,0 +1,4 @@
+package com.intelink.compproj.entity;
+
+public class Project {
+}
